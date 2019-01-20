33ee2a6aa9ec9506185a04b63871feeb4c195d0e
Oktavianus Chua <ata.chai10@gmail.com>
2019-01-20 16:28:41 +0700

    add uncut quote feature on model

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,9 +1,17 @@
 class Quote < ApplicationRecord
 	def next_id
-    self.class.where('id > ?', self.id).pluck(:id).first
+    next_id = self.class.where('id > ?', self.id).pluck(:id).first
+    if !next_id
+    	next_id = self.class.order('id').pluck(:id).first
+    end
+    return next_id
   end
 
   def previous_id
-    self.class.where('id < ?', self.id).pluck(:id).last
+    prev_id = self.class.where('id < ?', self.id).pluck(:id).last
+    if !prev_id
+    	prev_id = self.class.order('id').pluck(:id).last
+    end
+    return prev_id
   end
 end
